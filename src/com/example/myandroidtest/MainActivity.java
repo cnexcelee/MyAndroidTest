b1e432a0cac730d2c3ad52d59afc1e9560ddc811
excelee <cnexcelee@gmail.com>
2015-04-02 23:13:43 +0800

    add login activity sharedpreferences

--- src/com/example/myandroidtest/MainActivity.java
+++ src/com/example/myandroidtest/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.myandroidtest;
 
 import net.tsz.afinal.FinalActivity;
 import net.tsz.afinal.annotation.view.ViewInject;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -14,17 +17,29 @@ public class MainActivity extends FinalActivity {
 	
 	private EditText edt_name;
 	private EditText edt_pwd;
-	@ViewInject(id=R.id.btn_login,click="loginBtnListener")Button btn_login;
+	private Button btn_login;
 	
+	
+	SharedPreferences sp = null;
+	Editor editor = null;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		
+		sp = getSharedPreferences("LoginSP", Context.MODE_PRIVATE);
+		editor = sp.edit();
 		edt_name = (EditText) findViewById(R.id.edit_name);
 		edt_pwd = (EditText) findViewById(R.id.edit_pwd);
+		btn_login = (Button) findViewById(R.id.btn_login);
+		
+		edt_name.setText(sp.getString("loginName", ""));
+		edt_pwd.setText(sp.getString("loginPwd", ""));
+
+		
+		
+		btn_login.setOnClickListener(loginBtnListener);
 		
 	}
 
@@ -32,12 +47,33 @@ public class MainActivity extends FinalActivity {
 	public OnClickListener loginBtnListener = new OnClickListener() {
 		@Override
 		public void onClick(View v) {
-			Toast.makeText(getApplicationContext(), "test", Toast.LENGTH_SHORT).show();
 			String name = edt_name.getText().toString();
 			String pwd = edt_pwd.getText().toString();
-			if("".equals(name)){
-				Toast.makeText(getApplicationContext(), "test", Toast.LENGTH_SHORT).show();
+			
+			if(!strValueCheck(name)){
+				Toast.makeText(getApplicationContext(), " name is not allowed null", Toast.LENGTH_SHORT).show();
+				return;
 			}
+			if(!strValueCheck(pwd)){
+				Toast.makeText(getApplicationContext(), " password is not allowed null", Toast.LENGTH_SHORT).show();
+				return;
+			}
+			editor.putString("loginName", name);
+			editor.putString("loginPwd", pwd);
+			
+			
 		}
 	};
+	/**
+	 * check the string is or not null
+	 * @param str
+	 * @return
+	 */
+	public boolean strValueCheck(String str){
+		if(str == null || "".equals(str)){
+			return false;
+		}else{
+			return true;
+		}
+	}
 }
